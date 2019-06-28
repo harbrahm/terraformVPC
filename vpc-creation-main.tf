df40bc180ee1cd8220ad04ecf554be2b44e73ead
harbrahm <48707113+harbrahm@users.noreply.github.com>
2019-06-28 13:00:00 +0530

    Update vpc-creation-main.tf

--- vpc-creation-main.tf
+++ vpc-creation-main.tf
@@ -141,3 +141,20 @@ output "vpc_id" {
  value = "${aws_vpc.main.id}"
 }
 
+output "aws_subnet" {
+ value = "${aws_subnet.main.id}"
+}
+
+output "aws_internet_gateway" {
+ value = "${aws_internet_gateway.main.id}"
+}
+
+output "aws_route_table" {
+ value = "${aws_route_table.main.id}"
+}
+
+output "aws_route_table_association" {
+ value = "${aws_route_table_association.main.id}"
+}
+
+
